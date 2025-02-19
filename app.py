bdf2599c3364ade81b4ca97c101a1c4b24132c02
Wagner Serafim <wagner.serafim8@gmail.com>
2025-02-18 22:58:50 -0300

    1st test with a real iclinic csv patients table

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from datetime import datetime
+import pandas as pd
 
 # Configuração do banco de dados
 DATABASE_URL = "mssql+pyodbc://Medizin_32373:658$JQxn@medxserver.database.windows.net:1433/MEDX31?driver=ODBC+Driver+17+for+SQL+Server"
@@ -20,18 +21,33 @@ session = SessionLocal()
 # Acessar a classe correspondente à tabela "Contatos"
 Contatos = Base.classes.Contatos  # Nome da tabela no banco
 
-# Criar um novo contato
-novo_contato = Contatos(
-    Nome="Matheus Ferro",
-    Sexo="M",
-    Nascimento=datetime.strptime("27/01/1988", "%d/%m/%Y")  # Converter string para data
-)
+df = pd.read_csv("C:\Users\WJSur\Documents\iclinic_files\06-12-2024-patient.csv")
 
-# Adicionar à sessão e salvar no banco
-session.add(novo_contato)
+for index,item in df.items():
+
+    if item["birthdate"][index] == "":
+        birthday = datetime.strptime("01/01/1900", "%d/%m/%Y")
+    else:
+        birthday = item["birthdate"][index]
+    
+    if item["gender"][index] == "":
+        sex = "M"
+    else:
+        sex = item["gender"][index]
+
+
+    novo_contato = Contatos(
+        patient_id = item["patient_id"][index],
+        name = item["name"][index],
+        birthdate = birthday,
+        gender = sex
+    )
+
+    session.add(novo_contato)
+
 session.commit()
 
-print("Novo contato inserido com sucesso!")
+print("Novos contato inserido com sucesso!")
 
 # Fechar sessão
 session.close()
