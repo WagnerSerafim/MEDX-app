ece729c31afe140ac9a49372884ac82746baa5d4
Wagner Serafim <wagner.serafim8@gmail.com>
2025-02-19 23:32:19 -0300

    Updated: Iclinic patients

--- iclinic/patients.py
+++ iclinic/patients.py
@@ -39,14 +39,32 @@ for index,row in df.iterrows():
     else:
         sex = row["gender"]
 
+    if pd.isna(row["number"]):
+        address = row["address"]
+    else:
+        address = row["address"]+" "+row["number"]
 
     novo_contato = Contatos(
         Nome = truncate_value(row["name"], 50),
         Nascimento = birthday,
-        Sexo = sex
+        Sexo = sex.upper(),
+        RG = row["rg"],
+        Celular = row["mobile_phone"],
+        Email = row["email"],
+        Profissão = row["occupation"],
+        Observações = row["observation"]
     )
     setattr(novo_contato, "Id do Cliente", row["patient_id"])
-    print(f"Nome: {row['name']} ({len(str(row['name']))} caracteres)")
+    setattr(novo_contato, "CPF/CGC", row["cpf"])
+    setattr(novo_contato, "Telefone Residencial", row["home_phone"])
+    setattr(novo_contato, "Cep Residencial", row["zip_code"])
+    setattr(novo_contato, "Endereço Residencial", address)
+    setattr(novo_contato, "Endereço Comercial", row["complement"])
+    setattr(novo_contato, "Bairro Residencial", row["neighborhood"])
+    setattr(novo_contato, "Cidade Residencial", row["city"])
+    setattr(novo_contato, "Estado Residencial", row["state"])
+    setattr(novo_contato, "País Residencial", row["country"])
+    
     session.add(novo_contato)
 
 session.commit()
