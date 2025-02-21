9e8222ae61bc1936a4b2ec95b55052bae933279d
Wagner Serafim <wagner.serafim8@gmail.com>
2025-02-21 12:42:26 -0300

    Added: linx historico e hidoctor historico excel

--- iclinic/patients.py
+++ iclinic/patients.py
@@ -9,67 +9,65 @@ def truncate_value(value, max_length):
     """Se o valor for maior que max_length, ele será truncado"""
     if pd.isna(value):
         return None
-    return str(value)[:max_length]  # Mantém só os primeiros caracteres permitidos
+    return str(value)[:max_length] 
+
 
 DATABASE_URL = "mssql+pyodbc://Medizin_32373:658$JQxn@medxserver.database.windows.net:1433/MEDX31?driver=ODBC+Driver+17+for+SQL+Server"
 
 engine = create_engine(DATABASE_URL)
 
 Base = automap_base()
-Base.prepare(engine, reflect=True)
+Base.prepare(autoload_with=engine) 
 
-# Criar sessão
 SessionLocal = sessionmaker(bind=engine)
 session = SessionLocal()
 
-# Acessar a classe correspondente à tabela "Contatos"
 Contatos = Base.classes.Contatos
 
 df = pd.read_csv(r"C:\Users\Wagner Serafim\Documents\06-12-2024-patient.csv", sep=None, engine='python')
 
-for index,row in df.iterrows():
+for index, row in df.iterrows():
 
     if pd.isna(row["birthdate"]) or row["birthdate"] == "":
         birthday = datetime.strptime("01/01/1900", "%d/%m/%Y")
     else:
         birthday = row["birthdate"]
-    
-    if pd.isna(row["gender"]) or row["gender"] == "":
-        sex = "M"
-    else:
-        sex = row["gender"]
 
-    if pd.isna(row["number"]):
+    sex = "M" if pd.isna(row["gender"]) or row["gender"] == "" else row["gender"].upper()
+
+    if pd.isna(row["number"]) or row["number"] == "":
         address = row["address"]
     else:
-        address = row["address"]+" "+row["number"]
+        number = str(row["number"]) 
+        address = f"{row['address']} {number}"  
+
 
     novo_contato = Contatos(
-        Nome = truncate_value(row["name"], 50),
-        Nascimento = birthday,
-        Sexo = sex.upper(),
-        RG = row["rg"],
-        Celular = row["mobile_phone"],
-        Email = row["email"],
-        Profissão = row["occupation"],
-        Observações = row["observation"]
+        Nome=truncate_value(row["name"], 50),
+        Nascimento=birthday,
+        Sexo=sex,
+        RG=row["rg"][:25],
+        Celular=row["mobile_phone"],
+        Email=row["email"][:100],
+        Profissão=row["occupation"],
+        Observações=row["observation"]
     )
+
     setattr(novo_contato, "Id do Cliente", row["patient_id"])
     setattr(novo_contato, "CPF/CGC", row["cpf"])
     setattr(novo_contato, "Telefone Residencial", row["home_phone"])
     setattr(novo_contato, "Cep Residencial", row["zip_code"])
     setattr(novo_contato, "Endereço Residencial", address)
     setattr(novo_contato, "Endereço Comercial", row["complement"])
-    setattr(novo_contato, "Bairro Residencial", row["neighborhood"])
+    setattr(novo_contato, "Bairro Residencial", row["neighborhood"][:25])
     setattr(novo_contato, "Cidade Residencial", row["city"])
-    setattr(novo_contato, "Estado Residencial", row["state"])
-    setattr(novo_contato, "País Residencial", row["country"])
+    setattr(novo_contato, "Estado Residencial", row["state"][:2])
+    setattr(novo_contato, "País Residencial", row["country"][:50])
     
     session.add(novo_contato)
 
 session.commit()
 
-print("Novos contato inserido com sucesso!")
+print("Novos contatos inseridos com sucesso!")
 
-# Fechar sessão
 session.close()
---
+++ linx/historico_segmentos.py
@@ -0,0 +1,88 @@
+from bs4 import BeautifulSoup
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from datetime import datetime
+import pandas as pd
+from striprtf.striprtf import rtf_to_text
+import urllib
+import os
+
+def get_historic(row):
+    historic_columns = {
+        "QueixaPrincipal", "HistoriaMolestiaAtual", "HistoriaMorbidaPregressa", "AntecedenteMorbidoFamiliar", 
+        "MedicamentoEmUso", "ExameFisico", "HistoriaMorbidaSocial", "AntecedentesObstetricos", "AntecedentesMamarios", 
+        "AntecedentesContraceptivos", "ExameSegmentar", "Mama", "Vulva", "Especular", "Toque", "DescricaoCicloMenstrual"
+    }
+
+    filtered_data = row.dropna().filter(items=historic_columns)
+    cleaned_data = {col: str(val).replace("_x000D_", "<br>").strip() for col, val in filtered_data.items()}
+    return "<br>".join(f"{col}: {val}" for col, val in cleaned_data.items() if val)
+
+sid = input("Informe o SoftwareID: ")
+password = urllib.parse.quote_plus(input("Informe a senha: "))
+dbase= input("Informe o DATABASE: ")
+
+#DATABASE_URL = "mssql+pyodbc://Medizin_32373:658$JQxn@medxserver.database.windows.net:1433/MEDX31?driver=ODBC+Driver+17+for+SQL+Server"    #DEBUG
+DATABASE_URL = f"mssql+pyodbc://Medizin_{sid}:{password}@medxserver.database.windows.net:1433/{dbase}?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=no"
+
+engine = create_engine(DATABASE_URL)
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+HistoricoClientes = getattr(Base.classes, "Histórico de Clientes")
+
+historico_excel = input("Arquivo Excel Seguimentos Gerais Normais: ").strip()  
+prontuario_excel = input("Arquivo Excel Seguimentos Gerais: ").strip()  
+log_folder = input("Informe a pasta onde deseja salvar o arquivo de log: ").strip()
+
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+df_historic = pd.read_excel(historico_excel)  
+df_medical_record = pd.read_excel(prontuario_excel)  
+
+mapping_id_patient = pd.Series(df_medical_record["PacienteID"].values, index=df_medical_record["SeguimentoGeralNormalID"]).to_dict()
+
+log_data = []
+
+for index, row in df_medical_record.iterrows():
+    if row["SeguimentoGeralNormalID"] == 0:
+        continue
+
+    historic_data = df_historic[df_historic["SeguimentoGeralNormalID"] == row["SeguimentoGeralNormalID"]]
+
+    if historic_data.empty:
+        formatted_historic = "Sem histórico disponível"
+    else:
+        formatted_historic = get_historic(historic_data.iloc[0])
+    
+    novo_historico = HistoricoClientes( 
+        Histórico=formatted_historic, 
+        Data=row["DataConsulta"]  
+    )    
+    setattr(novo_historico, "Id do Cliente", row["PacienteID"]) 
+    setattr(novo_historico, "Id do Usuário", 0)
+    setattr(novo_historico, "Id do Histórico", (0 - row["SeguimentoGeralID"] ))
+
+    session.add(novo_historico)
+
+    log_data.append({
+        "Id Cliente": row["PacienteID"],
+        "Id Histórico": 0 - row["SeguimentoGeralID"],
+        "Data": row["DataConsulta"],
+        "Histórico": formatted_historic
+    })
+
+session.commit()
+session.close()
+
+log_df = pd.DataFrame(log_data)
+log_file_path = os.path.join(log_folder, "arquivo_log.xlsx")
+log_df.to_excel(log_file_path, index=False)
+
+print(f"✅ Novos históricos inseridos com sucesso! O arquivo de log foi salvo em: {log_file_path}")
