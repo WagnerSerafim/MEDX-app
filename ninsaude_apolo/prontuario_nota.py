a9391d6475063405f8c3bbae1de0226c1831f48c
Wagner Serafim <wagner.serafim8@gmail.com>
2025-05-28 01:23:32 -0300

    Added: ninsaude_apolo

---
+++ ninsaude_apolo/prontuario_nota.py
@@ -0,0 +1,112 @@
+from datetime import datetime
+import glob
+import os
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import pandas as pd
+import urllib
+from utils.utils import is_valid_date, exists, create_log, truncate_value
+
+sid = input("Informe o SoftwareID: ")
+password = urllib.parse.quote_plus(input("Informe a senha: "))
+dbase = input("Informe o DATABASE: ")
+path_file = input("Informe o caminho da pasta que contém os arquivos: ")
+
+print("Conectando no Banco de Dados...")
+DATABASE_URL = f"mssql+pyodbc://Medizin_{sid}:{password}@medxserver.database.windows.net:1433/{dbase}?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=no"
+
+engine = create_engine(DATABASE_URL)
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+HistoricoCliente = getattr(Base.classes, "Histórico de Clientes")
+
+print("Sucesso! Inicializando migração de Históricos...")
+
+todos_arquivos = glob.glob(f'{path_file}/*prontuario_nota.xml')
+
+df = pd.read_xml(todos_arquivos[0])
+df = df.replace('None', '')
+
+log_folder = path_file
+
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+log_data = []
+inserted_cont=0
+not_inserted_data = []
+not_inserted_cont = 0
+
+for _, row in df.iterrows():
+
+    if row["Paciente"] == None or row["Paciente"] == '' or row["Paciente"] == 'None':
+        not_inserted_cont +=1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = 'Id do Cliente vazio'
+        not_inserted_data.append(row_dict)
+        continue
+    else:
+        id_patient = 0 - (row["Paciente"])
+
+    id_record = -400 - row['Codigounico']
+    existing_record = exists(session, id_record, "Id do Histórico", HistoricoCliente)
+    if existing_record:
+        not_inserted_cont +=1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = f'Id do Histórico {id_record} já existe'
+        not_inserted_data.append(row_dict)
+        continue
+
+    if is_valid_date(f"{row['Data']} {row['Hora']}", "%Y-%m-%d %H:%M:%S"):
+        date = f"{row['Data']} {row['Hora']}"
+    else:
+        date = '01/01/1900 00:00'
+
+    if row['Anotacao'] is None or row['Anotacao'] == '' or row['Anotacao'] == 'None':
+        not_inserted_cont += 1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = 'Histórico vazio'
+        not_inserted_data.append(row_dict)
+        continue
+    else:
+        record = f"Anotação de Prontuário: {row['Anotacao']}"
+
+    new_record = HistoricoCliente(
+        Data = date,
+        Histórico = record
+    )
+
+    setattr(new_record, "Id do Cliente", id_patient)
+    setattr(new_record, "Id do Histórico", id_record)
+    setattr(new_record, "Id do Usuário", 0)
+
+    log_data.append({
+        "Id do Cliente": id_patient,
+        "Id do Histórico": id_record,
+        "Data": '01/01/1900',
+        "Histórico": record,
+        "Id do Usuário": 0
+    })
+
+    session.add(new_record)
+
+    inserted_cont += 1
+    if inserted_cont % 100 == 0:
+        session.commit()
+
+session.commit()
+
+print(f"{inserted_cont} novos históricos foram inseridos com sucesso!")
+if not_inserted_cont > 0:
+    print(f"{not_inserted_cont} históricos não foram inseridos, verifique o log para mais detalhes.")
+
+session.close()
+
+create_log(log_data, log_folder, "log_inserted_prontuario_nota.xlsx")
+create_log(not_inserted_data, log_folder, "log_not_inserted_prontuario_nota.xlsx")
