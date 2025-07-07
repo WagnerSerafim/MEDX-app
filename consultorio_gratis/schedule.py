8161a35e2c08adadc8e42aa0b26d2d6b3b1d0c73
Wagner Serafim <wagner.serafim8@gmail.com>
2025-07-06 21:58:10 -0300

    clinica-gratis schedule

---
+++ consultorio_gratis/schedule.py
@@ -0,0 +1,141 @@
+import csv
+import glob
+import json
+import os
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import pandas as pd
+import urllib
+from utils.utils import is_valid_date, exists, create_log
+from datetime import datetime
+
+
+sid = input("Informe o SoftwareID: ")
+password = urllib.parse.quote_plus(input("Informe a senha: "))
+dbase = input("Informe o DATABASE: ")
+path_file = input("Informe o caminho da pasta que contém os arquivos: ")
+
+print("Conectando no Banco de Dados...")
+DATABASE_URL = f"mssql+pyodbc://Medizin_{sid}:{password}@medxserver.database.windows.net:1433/{dbase}?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=no"
+
+engine = create_engine(DATABASE_URL)
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+Agenda = getattr(Base.classes, "Agenda")
+Contatos = getattr(Base.classes, "Contatos")
+
+print("Sucesso! Inicializando migração de Agendamentos...")
+
+todos_arquivos = glob.glob(f'{path_file}/agendamentos.csv')
+
+csv.field_size_limit(1000000000)  
+df = pd.read_csv(todos_arquivos[0], sep=',')
+df = df.replace('None', '')
+
+log_folder = path_file
+
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+log_data = []
+inserted_cont=0
+not_inserted_data = []
+not_inserted_cont = 0
+
+for _, row in df.iterrows():
+    
+    date_str = row['AGENDAMENTO_INICIO']
+    if date_str is None or pd.isna(date_str) or date_str == '':
+        not_inserted_cont += 1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = 'Data ou Hora vazia'
+        row_dict['Timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        not_inserted_data.append(row_dict)
+        continue
+
+    date_obj = datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
+    date_str = date_obj.strftime('%Y-%m-%d %H:%M')
+    if is_valid_date(date_str, '%Y-%m-%d %H:%M'):
+        start_time = date_str
+        
+        end_str = row['AGENDAMENTO_FIM']
+        if end_str is None or pd.isna(end_str) or end_str == '':
+            not_inserted_cont += 1
+            row_dict = row.to_dict()
+            row_dict['Motivo'] = 'Data ou Hora de fim vazia'
+            row_dict['Timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            not_inserted_data.append(row_dict)
+            continue
+        end_date_obj = datetime.strptime(end_str, '%d/%m/%Y %H:%M:%S')
+        end_time = end_date_obj.strftime('%Y-%m-%d %H:%M') 
+    else:
+        not_inserted_cont += 1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = 'Data ou Hora inválida'
+        row_dict['Timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        not_inserted_data.append(row_dict)
+        continue
+
+    if row['PACIENTE_CODIGO'] in [None, '', 'None'] or pd.isna(row['PACIENTE_CODIGO']):
+        not_inserted_cont +=1
+        row_dict = row.to_dict()
+        row_dict['Motivo'] = 'Id do paciente vazio'
+        not_inserted_data.append(row_dict)
+        continue
+    else:
+        patient = exists(session, row['PACIENTE_CODIGO'], 'Referências', Contatos)
+        if not patient:
+            not_inserted_cont += 1
+            row_dict = row.to_dict()
+            row_dict['Motivo'] = 'Paciente não encontrado'
+            not_inserted_data.append(row_dict)
+            continue
+        else:
+            id_patient = getattr(patient, "Id do Contato")
+
+    id_user = 1
+
+    description = f'{patient.Nome} - {row['AGENDAMENTO_STATUS']}'
+
+    new_schedulling = Agenda(
+        Descrição=description,
+        Início=start_time,
+        Final=end_time,
+        Status=1,
+    )
+
+    setattr(new_schedulling, "Vinculado a", id_patient)
+    setattr(new_schedulling, "Id do Usuário", id_user)
+    
+    log_data.append({
+        "Vinculado a": id_patient,
+        "Id do Usuário": id_user,
+        "Início": start_time,
+        "Final": end_time,
+        "Descrição": description,
+        "Status" : 1
+    })
+
+    session.add(new_schedulling)
+
+    inserted_cont+=1
+
+    if inserted_cont % 1000 == 0:
+        session.commit()
+
+session.commit()
+
+print(f"{inserted_cont} novos agendamentos foram inseridos com sucesso!")
+if not_inserted_cont > 0:
+    print(f"{not_inserted_cont} agendamentos não foram inseridos, verifique o log para mais detalhes.")
+
+session.close()
+
+create_log(log_data, log_folder, "log_inserted_schedule.xlsx")
+create_log(not_inserted_data, log_folder, "log_not_inserted_schedule.xlsx")
